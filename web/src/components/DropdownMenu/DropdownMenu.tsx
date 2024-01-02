8abb9478a36b9c1acb38c05496c861fc8891220f
Richard Chung <iwannaplaygamesjp@gmail.com>
2024-01-02 02:35:05 +0000

    make drop down menu comp

---
+++ web/src/components/DropdownMenu/DropdownMenu.tsx
@@ -0,0 +1,10 @@
+const DropdownMenu = () => {
+  return (
+    <div>
+      <h2>{'DropdownMenu'}</h2>
+      <p>{'Find me in ./web/src/components/DropdownMenu/DropdownMenu.tsx'}</p>
+    </div>
+  )
+}
+
+export default DropdownMenu
