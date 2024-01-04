369428ee90bed5a0f93b9c16c44b6e6d7a671688
Richard Chung <iwannaplaygamesjp@gmail.com>
2024-01-04 04:15:04 +0000

    generate checkbox component

---
+++ web/src/components/CheckBox/CheckBox.stories.tsx
@@ -0,0 +1,26 @@
+// Pass props to your component by passing an `args` object to your story
+//
+// ```tsx
+// export const Primary: Story = {
+//  args: {
+//    propName: propValue
+//  }
+// }
+// ```
+//
+// See https://storybook.js.org/docs/react/writing-stories/args.
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+import CheckBox from './CheckBox'
+
+const meta: Meta<typeof CheckBox> = {
+  component: CheckBox,
+  tags: ['autodocs'],
+}
+
+export default meta
+
+type Story = StoryObj<typeof CheckBox>
+
+export const Primary: Story = {}
---
+++ web/src/components/CheckBox/CheckBox.tsx
@@ -0,0 +1,10 @@
+const CheckBox = () => {
+  return (
+    <div>
+      <h2>{'CheckBox'}</h2>
+      <p>{'Find me in ./web/src/components/CheckBox/CheckBox.tsx'}</p>
+    </div>
+  )
+}
+
+export default CheckBox
